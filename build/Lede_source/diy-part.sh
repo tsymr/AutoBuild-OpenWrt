bd17a0caad0fe48a5298346803420c276db6754e
281677160 <66651997+281677160@users.noreply.github.com>
2021-06-17 20:16:48 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -22,7 +22,7 @@ sed -i "/uci commit system/i\uci set system.@system[0].hostname='x86-64-OpenWrt'
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                            # 设置密码为空
 
-#sed -i 's/PATCHVER:=5.4/PATCHVER:=4.19/g' target/linux/x86/Makefile                              # 修改内核版本为4.19
+#sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' target/linux/x86/Makefile                              # 修改内核版本为5.4
 
 
 # 修改插件名字
