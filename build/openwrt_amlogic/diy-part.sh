b81280e962c27c4970a0595f70adbaf292cf3119
281677160 <66651997+281677160@users.noreply.github.com>
2021-06-30 16:47:24 +0800

    Update diy-part.sh

--- build/openwrt_amlogic/diy-part.sh
+++ build/openwrt_amlogic/diy-part.sh
@@ -19,7 +19,7 @@ sed -i "/uci commit system/i\uci set system.@system[0].hostname='Phicomm-N1'" $Z
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                            # 设置密码为空
 
-#sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' target/linux/x86/Makefile                              # 修改内核版本为5.4,默认内核5.10
+#sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/armvirt/Makefile                          # 修改内核版本为5.10,默认内核5.4
 
 
 # 设置打包固件的机型，内核组合（请看说明）
